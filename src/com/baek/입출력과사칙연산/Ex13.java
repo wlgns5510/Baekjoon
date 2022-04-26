7cba9f9382fa8ba58dd5f6baee9f84d29af46fde
5335510 <5335510@naver.com>
2022-04-27 01:38:46 +0900

    2022-04-27 01:38

--- src/com/baek/입출력과사칙연산/Ex13.java
+++ src/com/baek/입출력과사칙연산/Ex13.java
@@ -6,16 +6,16 @@ public class Ex13 {
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		int num1,num2;
-		num1 = sc.nextInt();
-		num2 = sc.nextInt();
+		int num1 = sc.nextInt();  //1
+		int num2 = sc.nextInt();  //2
 		
 		
+		System.out.println(num1 * (num2%10));  //3
+		System.out.println(num1 * ((num2%100-(num2%10))/10));  //4
+		System.out.println(num1 * ((num2-num2%100))/100);  //5
+		System.out.println(num1 * num2);  //6
 		
-		System.out.println(num1*(num2-));
-		System.out.println(num1 * num2);
 		sc.close();
-
 	}
 
 }
