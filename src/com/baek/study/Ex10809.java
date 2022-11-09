f59bbc10b3795f6f2b7067bd41489c0b1a044a01
5335510 <5335510@naver.com>
2022-11-09 16:44:36 +0900

    2022 11-09

---
+++ src/com/baek/study/Ex10809.java
@@ -0,0 +1,16 @@
+package com.baek.study;
+
+import java.util.Scanner;
+
+public class Ex10809 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		
+		
+		sc.close();
+
+	}
+
+}
