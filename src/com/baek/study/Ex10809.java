54ca79483f8ff817adf402acfe395baac3960fef
5335510 <5335510@naver.com>
2022-11-10 00:18:33 +0900

    2022 11-10

--- src/com/baek/study/Ex10809.java
+++ src/com/baek/study/Ex10809.java
@@ -7,8 +7,29 @@ public class Ex10809 {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		
+		String S = sc.nextLine();
+		
+		int[] abc = new int[26];
+		
+		for(int i=0; i<abc.length; i++) {
+			abc[i] = -1;
+		}
 		
 		
+		
+		 for(int i=0; i<S.length(); i++) {
+		 
+			 char get = S.charAt(i);
+			 
+			 if(abc[get - 'a'] == -1) {
+				 abc[get - 'a'] = i;
+			 }
+		 }
+		
+		for(int var : abc) {
+			System.out.print(var + " ");
+		}
+		 
 		sc.close();
 
 	}
