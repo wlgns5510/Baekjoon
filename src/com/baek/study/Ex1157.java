c4b41313f0343b4b74ef95f4d6bd0f729bc29ee9
5335510 <5335510@naver.com>
2022-11-14 00:47:50 +0900

    2022 11-14

--- src/com/baek/study/Ex1157.java
+++ src/com/baek/study/Ex1157.java
@@ -1,10 +1,33 @@
 package com.baek.study;
 
+import java.util.Scanner;
+
+
 public class Ex1157 {
+	static Scanner sc = new Scanner(System.in);
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		String str = sc.next();
+		String str2 = str.toUpperCase();
+// System.out.println((int)'Z'); // A = 65 Z = 90
+// apple
+		int[] numArr = new int[26];
 
+		for (int i = 0; i < str.length(); i++) {
+			numArr[(int) str2.charAt(i) - 65]++;
+		}
+// a 1 p 2 l 1 e 1
+		int max = 0;
+		char c = ' ';
+
+		for (int i = 0; i < numArr.length; i++) {
+			if (max < numArr[i]) {
+				max = numArr[i];
+				c = (char) (i + 65); // max == 2 / c == (char)P // mmaa
+			} else if (max == numArr[i]) {
+				c = '?';
+			}
+		}
+		System.out.println(c);
 	}
-
 }
