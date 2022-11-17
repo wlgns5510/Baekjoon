9436125e81bdacc61d00f9075a4cfb2280e75306
5335510 <5335510@naver.com>
2022-11-17 18:11:20 +0900

    2022 11-17

---
+++ src/com/baek/study/Ex1712.java
@@ -0,0 +1,14 @@
+package com.baek.study;
+
+import java.util.Scanner;
+
+public class Ex1712 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		sc.close();
+
+	}
+
+}
