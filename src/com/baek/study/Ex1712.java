7fe100cfc5d994012bdc0c0ac4ebd0fc25dc7f36
5335510 <5335510@naver.com>
2022-11-19 14:42:01 +0900

    2022 11-19

--- src/com/baek/study/Ex1712.java
+++ src/com/baek/study/Ex1712.java
@@ -7,8 +7,32 @@ public class Ex1712 {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		
+		int A = sc.nextInt();
+		int B = sc.nextInt();
+		int C = sc.nextInt();
+		
+		int sum = A+B;
+		int count = 1;
 		sc.close();
+		
+		
+		while(sum > C) {
+			count ++;
+			B+=B;
+			C+=C;
+			System.out.println(B);
+			System.out.println(C);
+			if(sum < C) {
+				break;
+			}
+		}
+		System.out.println(count);
 
 	}
 
 }
+
+// A		 B		 C				손익분기점
+//1대 1000	70		170			1070>170	X
+//2대 1000	140		240			1140>240	X
+//3대 1000	210		410			1210>410	X
